21268d17ab17508b1e91eaec407a29141e3add2b
Eric Drake <edrake@doublelglobal.com>
2021-05-22 12:52:24 -0600

    Fixed some css conflicts from Laravel Breeze installation

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,8 @@
         <title>@yield('title')</title>
         <meta name="keywords" content="@yield('keywords')" />
         <meta name="description" content="@yield('description')" />
-        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" />
+        {{-- <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" /> --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/common.css') }}" />
         <script src="https://kit.fontawesome.com/3207795e71.js" crossorigin="anonymous"></script>
     </head>
@@ -102,6 +103,7 @@
 			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 			  crossorigin="anonymous">
         </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
         <script src="{{ asset('js/common.js') }}"></script>
         @show
     </body>
